e5e5824e8070004a51d42301e18f6178d79f305b
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2025-02-18 02:36:27 +0300

    Restructure `next_move` and store moves as (u8, u8)
    `next_move` is now structured such that there is one point of determining
    the next move's value.
    
    Moves also are stored as (u8, u8) tuple instead of
    `Move {u8, (usize, usize)}`

--- src/solve/mod.rs
+++ src/solve/mod.rs
@@ -8,11 +8,11 @@ use crate::{
 #[derive(Debug)]
 pub struct Move {
     pub val: u8,
-    pub pos: (usize, usize),
+    pub pos: (u8, u8),
 }
 
 impl<const N: usize> Grid<N> {
-    pub fn next_move(self: &Self, domain: &Domain<N>, _cages: &[Cage]) -> Option<Move> {
+    pub fn next_move(self: &Self, domain: &Domain<N>) -> Option<Move> {
         let mut next: Option<Move> = None;
 
         for x in 0..N {
@@ -21,29 +21,30 @@ impl<const N: usize> Grid<N> {
                     continue;
                 }
 
-                let first_true = match domain[x][y].iter().position(|&v| v) {
-                    Some(v) => v as u8,
+                match next {
+                    Some(ref old)
+                        if domain[x][y].iter().filter(|&&b| b).count()
+                            < domain[old.pos.0 as usize][old.pos.1 as usize]
+                                .iter()
+                                .filter(|&&b| b)
+                                .count() =>
+                    {
+                        ()
+                    }
+                    None => (),
+                    _ => continue,
+                }
+
+                let val = domain[x][y].iter().position(|&v| v).map(|v| v as u8);
+                let val = match val {
+                    Some(val) => val,
                     None => continue,
                 };
 
-                match next {
-                    Some(old)
-                        if domain[x][y].iter().filter(|&&b| b).count()
-                            < domain[old.pos.0][old.pos.1].iter().filter(|&&b| b).count() =>
-                    {
-                        next = Some(Move {
-                            val: first_true + 1,
-                            pos: (x, y),
-                        });
-                    }
-                    None => {
-                        next = Some(Move {
-                            val: first_true + 1,
-                            pos: (x, y),
-                        });
-                    }
-                    _ => continue,
-                }
+                next = Some(Move {
+                    val,
+                    pos: (x as u8, y as u8),
+                });
             }
         }
 
@@ -51,15 +52,14 @@ impl<const N: usize> Grid<N> {
     }
 
     pub fn solve(mut self, cages: &[Cage]) -> Option<Self> {
-        let mut steps: Vec<(Move, Domain<N>)> = Vec::new();
+        let mut steps: Vec<((u8, u8), Domain<N>)> = Vec::new();
         let mut domain: Domain<N>;
         let mut backtrack = false;
         loop {
             if backtrack {
                 let prev;
                 (prev, domain) = steps.pop()?;
-                self[prev.pos.0][prev.pos.1] = 0;
-                domain[prev.pos.0][prev.pos.1][prev.val as usize - 1] = false;
+                self[prev.0 as usize][prev.1 as usize] = 0;
                 backtrack = false;
             } else {
                 domain = Domain::new(&self, cages);
@@ -74,7 +74,7 @@ impl<const N: usize> Grid<N> {
                 _ => (),
             };
 
-            let mv = match self.next_move(&domain, cages) {
+            let mv = match self.next_move(&domain) {
                 Some(mv) => mv,
                 None => {
                     backtrack = true;
@@ -82,8 +82,9 @@ impl<const N: usize> Grid<N> {
                 }
             };
 
-            self[mv.pos.0][mv.pos.1] = mv.val;
-            steps.push((mv, domain));
+            self[mv.pos.0 as usize][mv.pos.1 as usize] = mv.val + 1;
+            domain[mv.pos.0 as usize][mv.pos.1 as usize][mv.val as usize] = false;
+            steps.push((mv.pos, domain));
         }
     }
 }
